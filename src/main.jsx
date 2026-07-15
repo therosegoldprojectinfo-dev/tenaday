269f07e70cda2e7f1f04e8537c9b8caa86a0bdd9
therosegoldprojectinfo-dev <therosegoldproject.info@gmail.com>
2026-07-15 10:28:02 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import PrivacyPolicy from './screens/PrivacyPolicy.jsx'
+import TermsAndConditions from './screens/TermsAndConditions.jsx'
 import './index.css'
+
+const path = window.location.pathname
+
+let root
+
+if (path === '/privacy') {
+  root = <PrivacyPolicy onBack={() => window.location.href = '/'} />
+} else if (path === '/terms') {
+  root = <TermsAndConditions onBack={() => window.location.href = '/'} />
+} else {
+  root = <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {root}
   </React.StrictMode>
 )
