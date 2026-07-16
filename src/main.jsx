d8b1f6631f93babed9f3ea8089beef928ecfdf86
therosegoldprojectinfo-dev <therosegoldproject.info@gmail.com>
2026-07-16 13:30:24 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import PrivacyPolicy from './screens/PrivacyPolicy.jsx'
 import TermsAndConditions from './screens/TermsAndConditions.jsx'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 
 const path = window.location.pathname
@@ -20,5 +21,6 @@ if (path === '/privacy') {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {root}
+    <Analytics />
   </React.StrictMode>
 )
