36efca441a8757bf56c1bcfa6551685b245e7ae0
therosegoldprojectinfo-dev <therosegoldproject.info@gmail.com>
2026-07-31 11:01:12 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -166,6 +166,7 @@ export default function App() {
               }
 
               if (signupLang) setLang(signupLang)
+              setActivated(true)
               setOnboarded(true)
             }}
           />
